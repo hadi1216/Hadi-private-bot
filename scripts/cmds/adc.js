44c8f97b5fece4f3361ecdefa63c169bd8dc8ead
shamimbroh <164640461+shamimbroh@users.noreply.github.com>
2024-05-08 15:01:34 +0600

    adc.js

--- scripts/cmds/adc.js
+++ scripts/cmds/adc.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	
 onStart: async function({ api, event, args }) {
-  const permission = ["100025136351262", "100085947075503"];
+  const permission = ["100064502005991", "100085947075503"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("❌ | You aren't allowed to use this command.", event.threadID, event.messageID);
  
